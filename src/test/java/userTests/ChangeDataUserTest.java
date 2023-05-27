5d6f80c4311a572cd4dd82ce63e4e58b1b4fccb5
mshulgina <mshulgina@cinimex.ru>
2023-05-27 20:50:30 +0500

    добавлены тесты на изменение данных пользоватиля: с авторизацией и без

---
+++ src/test/java/userTests/ChangeDataUserTest.java
@@ -0,0 +1,74 @@
+package userTests;
+
+import baseUtil.ClientCredentials;
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import user.User;
+import user.UserClient;
+import user.UserInfo;
+import static org.apache.http.HttpStatus.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@DisplayName("Изменение данных пользователя")
+public class ChangeDataUserTest {
+    private User user;
+    private User userUpdate;
+    private User userSecond;
+    private UserClient userClient;
+    private String accessToken;
+
+    @Before
+    public void setUp() {
+        user = UserInfo.getUser();
+        userUpdate = UserInfo.getUserUpdate();
+        userSecond = UserInfo.getUserSecond();
+        userClient = new UserClient();
+    }
+
+    @Test
+    @DisplayName("Изменение имени пользователя с авторизацией.")
+    @Description("Изменение имени пользователя с авторизацией. Проверка успешного ответа сервера.")
+    public void changeUserData() {
+        userClient.createUser(ClientCredentials.from(user));
+        ValidatableResponse responseLogin = userClient.loginUser(ClientCredentials.from(user));
+        accessToken = responseLogin.extract().path("accessToken");
+        ValidatableResponse responseUpdateUser = userClient.changeUser(accessToken, ClientCredentials.from(userUpdate));
+        assertEquals(SC_OK, responseUpdateUser.extract().statusCode());
+        assertTrue(responseUpdateUser.extract().path("success"));
+    }
+
+    @Test
+    @DisplayName("Изменение данных пользователя без авторизации")
+    @Description("Изменение данных пользователя без авторизации. Проверка неуспешного ответа от сервера.")
+    public void changeUserDataWithoutLogin() {
+        ValidatableResponse responseCreate = userClient.createUser(ClientCredentials.from(user));
+        accessToken = responseCreate.extract().path("accessToken");
+        ValidatableResponse responseUpdateUser = userClient.changeUser("", ClientCredentials.from(userUpdate));
+        assertEquals(SC_UNAUTHORIZED, responseUpdateUser.extract().statusCode());
+        assertEquals("You should be authorised", responseUpdateUser.extract().path("message"));
+    }
+
+    @Test
+    @DisplayName("Изменение данных пользователя с существующим email")
+    @Description("Изменение данных пользователя с существующим email. Проверка неуспешного ответа от сервера.")
+    public void changeUserDataExistEmail() {
+        ValidatableResponse responseCreate = userClient.createUser(ClientCredentials.from(user));
+        ValidatableResponse responseCreateSecondUser = userClient.createUser(ClientCredentials.from(userSecond));
+        accessToken = responseCreate.extract().path("accessToken");
+        String accessTokenSecond = responseCreateSecondUser.extract().path("accessToken");
+        ValidatableResponse responseUpdateUser = userClient.changeUser(accessToken, ClientCredentials.from(userSecond));
+        assertEquals(SC_FORBIDDEN, responseUpdateUser.extract().statusCode());
+        assertEquals("User with such email already exists", responseUpdateUser.extract().path("message"));
+        userClient.deleteUser(accessTokenSecond);
+    }
+
+    @After
+    public void cleanUp() {
+        userClient.deleteUser(accessToken);
+    }
+}
